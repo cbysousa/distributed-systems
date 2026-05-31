1ee2242fa77937a48dcf1aaae24aa03b163d8e1a
Pedro Gabriel Castelo Garcez <pedrogcgds@gmail.com>
2026-05-31 16:37:18 -0300

    Create protobuf initial messages and init gateway with multicast udp discovery

---
+++ gateway/cmd/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+type DataSource struct {
+	Controllable bool
+	Address      string
+	Name         string
+}
+
+var sources []DataSource
+
+func init() {
+	sources = make([]DataSource, 0, 5)
+	addr, err := net.ResolveUDPAddr("udp4", ":0")
+	if err != nil {
+		panic(err)
+	}
+	conn, err := net.ListenUDP("udp4", &net.UDPAddr{
+		IP:   net.IPv4zero,
+		Port: 0,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	msg := "DISCOVER"
+	_, err = conn.WriteToUDP([]byte(msg), addr)
+	if err != nil {
+		panic(err)
+	}
+	buf := make([]byte, 1024)
+	for {
+		n, rAddr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			break
+		}
+		parts := strings.Split(string(buf[:n]), "|")
+		if len(parts) < 2 {
+			continue
+		}
+		src := DataSource{
+			Controllable: parts[1] == "1",
+			Address:      rAddr.IP.String(),
+			Name:         parts[0],
+		}
+		sources = append(sources, src)
+	}
+}
+
+func main() {
+	for _, s := range sources {
+		fmt.Println(s.Address)
+	}
+}
