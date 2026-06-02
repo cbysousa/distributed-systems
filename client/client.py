e8bb139e8796ce3037e02e797578d3eecfad01c2
Caio Braga <8caiobraga8@gmail.com>
2026-06-02 08:41:40 -0300

    Basic analytic client

--- client/client.py
+++ client/client.py
@@ -29,6 +29,48 @@ def main():
         print("Verifique se o container do Gateway está em execução.")
         return
 
+    try:
+        msg = receive(sock)
+        if msg:
+            print(msg, end="")
+
+        msg = receive(sock)
+        if msg:
+            print(msg, end="")
+
+        while True:
+            print("\nMenu:")
+            print("1 - Listar fontes descobertas")
+            print("2 - Ajuda")
+            print("0 - Sair")
+
+            option = input("Escolha uma opção: ").strip()
+
+            if option == "1":
+                command = "LIST"
+            elif option == "2":
+                command = "HELP"
+            elif option == "0":
+                response = send_command(sock, "EXIT")
+                if response:
+                    print(response, end="")
+                break
+            else:
+                print("Opção inválida.")
+                continue
+
+            response = send_command(sock, command)
+
+            if response is None:
+                print("Conexão encerrada pelo Gateway.")
+                break
+
+            print("\nResposta do Gateway:")
+            print(response, end="")
+
+    finally:
+        sock.close()
+        print("Cliente encerrado.")
 
 
 if __name__ == "__main__":
